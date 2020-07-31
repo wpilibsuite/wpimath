00cb46beed8908ac0251e6b9a3abbb8faa50d5c7
Tyler Veness <calcmogul@gmail.com>
2020-07-30 22:16:43 -0700

    Set up wpiformat (#3)

--- src/dev/native/cpp/main.cpp
+++ src/dev/native/cpp/main.cpp
@@ -1,3 +1,8 @@
-int main() {
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
-}
+int main() {}
